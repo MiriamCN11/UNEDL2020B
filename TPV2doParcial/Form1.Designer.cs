1dd0868609dd851e30d6ed8f0c150451f4b61e4b
Miriam Castañeda <mncn110599@hotmail.com>
2020-11-27 18:37:30 -0600

    terminado

--- TPV2doParcial/Form1.Designer.cs
+++ TPV2doParcial/Form1.Designer.cs
@@ -29,12 +29,354 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblLocalidadI = new System.Windows.Forms.Label();
+            this.lblOpcI = new System.Windows.Forms.Label();
+            this.lblLocalidadB = new System.Windows.Forms.Label();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.lblPCom = new System.Windows.Forms.Label();
+            this.lblPass = new System.Windows.Forms.Label();
+            this.lblSisR = new System.Windows.Forms.Label();
+            this.lblM = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtLI = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtLB = new System.Windows.Forms.TextBox();
+            this.txtPC = new System.Windows.Forms.TextBox();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.cbxOpc = new System.Windows.Forms.ComboBox();
+            this.cbxSisR = new System.Windows.Forms.ComboBox();
+            this.btnInstalacion = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.btnBrowseB = new System.Windows.Forms.Button();
+            this.lstDatosI = new System.Windows.Forms.ListBox();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(208, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "---INSTALACION---";
+            // 
+            // lblLocalidadI
+            // 
+            this.lblLocalidadI.AutoSize = true;
+            this.lblLocalidadI.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLocalidadI.Location = new System.Drawing.Point(71, 60);
+            this.lblLocalidadI.Name = "lblLocalidadI";
+            this.lblLocalidadI.Size = new System.Drawing.Size(214, 24);
+            this.lblLocalidadI.TabIndex = 1;
+            this.lblLocalidadI.Text = "Localidad de inventario: ";
+            // 
+            // lblOpcI
+            // 
+            this.lblOpcI.AutoSize = true;
+            this.lblOpcI.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOpcI.Location = new System.Drawing.Point(71, 107);
+            this.lblOpcI.Name = "lblOpcI";
+            this.lblOpcI.Size = new System.Drawing.Size(222, 24);
+            this.lblOpcI.TabIndex = 2;
+            this.lblOpcI.Text = "Opciones de instalacion: ";
+            // 
+            // lblLocalidadB
+            // 
+            this.lblLocalidadB.AutoSize = true;
+            this.lblLocalidadB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLocalidadB.Location = new System.Drawing.Point(71, 157);
+            this.lblLocalidadB.Name = "lblLocalidadB";
+            this.lblLocalidadB.Size = new System.Drawing.Size(147, 24);
+            this.lblLocalidadB.TabIndex = 3;
+            this.lblLocalidadB.Text = "Localidad base: ";
+            // 
+            // lblIP
+            // 
+            this.lblIP.AutoSize = true;
+            this.lblIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblIP.Location = new System.Drawing.Point(71, 253);
+            this.lblIP.Name = "lblIP";
+            this.lblIP.Size = new System.Drawing.Size(116, 24);
+            this.lblIP.TabIndex = 4;
+            this.lblIP.Text = "Direccion IP:";
+            // 
+            // lblPCom
+            // 
+            this.lblPCom.AutoSize = true;
+            this.lblPCom.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPCom.Location = new System.Drawing.Point(71, 206);
+            this.lblPCom.Name = "lblPCom";
+            this.lblPCom.Size = new System.Drawing.Size(226, 24);
+            this.lblPCom.TabIndex = 5;
+            this.lblPCom.Text = "Puerto de comunicacion: ";
+            // 
+            // lblPass
+            // 
+            this.lblPass.AutoSize = true;
+            this.lblPass.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPass.Location = new System.Drawing.Point(71, 299);
+            this.lblPass.Name = "lblPass";
+            this.lblPass.Size = new System.Drawing.Size(222, 24);
+            this.lblPass.TabIndex = 6;
+            this.lblPass.Text = "Password de instalacion: ";
+            // 
+            // lblSisR
+            // 
+            this.lblSisR.AutoSize = true;
+            this.lblSisR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSisR.Location = new System.Drawing.Point(71, 346);
+            this.lblSisR.Name = "lblSisR";
+            this.lblSisR.Size = new System.Drawing.Size(224, 24);
+            this.lblSisR.TabIndex = 7;
+            this.lblSisR.Text = "Sistema de redundancia: ";
+            // 
+            // lblM
+            // 
+            this.lblM.AutoSize = true;
+            this.lblM.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblM.ForeColor = System.Drawing.Color.Red;
+            this.lblM.Location = new System.Drawing.Point(273, 58);
+            this.lblM.Name = "lblM";
+            this.lblM.Size = new System.Drawing.Size(20, 25);
+            this.lblM.TabIndex = 8;
+            this.lblM.Text = "*";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(287, 107);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "*";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Red;
+            this.label4.Location = new System.Drawing.Point(287, 344);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "*";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.Red;
+            this.label5.Location = new System.Drawing.Point(287, 297);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(20, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "*";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(208, 155);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(20, 25);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "*";
+            // 
+            // txtLI
+            // 
+            this.txtLI.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLI.Location = new System.Drawing.Point(324, 55);
+            this.txtLI.Name = "txtLI";
+            this.txtLI.Size = new System.Drawing.Size(159, 28);
+            this.txtLI.TabIndex = 14;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(324, 295);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(268, 28);
+            this.txtPassword.TabIndex = 15;
+            // 
+            // txtLB
+            // 
+            this.txtLB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLB.Location = new System.Drawing.Point(324, 152);
+            this.txtLB.Name = "txtLB";
+            this.txtLB.Size = new System.Drawing.Size(159, 28);
+            this.txtLB.TabIndex = 16;
+            // 
+            // txtPC
+            // 
+            this.txtPC.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPC.Location = new System.Drawing.Point(324, 202);
+            this.txtPC.Name = "txtPC";
+            this.txtPC.Size = new System.Drawing.Size(268, 28);
+            this.txtPC.TabIndex = 17;
+            // 
+            // txtIP
+            // 
+            this.txtIP.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtIP.Location = new System.Drawing.Point(324, 249);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(268, 28);
+            this.txtIP.TabIndex = 18;
+            // 
+            // cbxOpc
+            // 
+            this.cbxOpc.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxOpc.FormattingEnabled = true;
+            this.cbxOpc.Location = new System.Drawing.Point(324, 101);
+            this.cbxOpc.Name = "cbxOpc";
+            this.cbxOpc.Size = new System.Drawing.Size(268, 30);
+            this.cbxOpc.TabIndex = 19;
+            // 
+            // cbxSisR
+            // 
+            this.cbxSisR.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cbxSisR.FormattingEnabled = true;
+            this.cbxSisR.Location = new System.Drawing.Point(324, 339);
+            this.cbxSisR.Name = "cbxSisR";
+            this.cbxSisR.Size = new System.Drawing.Size(268, 30);
+            this.cbxSisR.TabIndex = 20;
+            // 
+            // btnInstalacion
+            // 
+            this.btnInstalacion.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnInstalacion.Location = new System.Drawing.Point(69, 413);
+            this.btnInstalacion.Name = "btnInstalacion";
+            this.btnInstalacion.Size = new System.Drawing.Size(238, 90);
+            this.btnInstalacion.TabIndex = 21;
+            this.btnInstalacion.Text = "Iniciar Instalacion";
+            this.btnInstalacion.UseVisualStyleBackColor = true;
+            this.btnInstalacion.Click += new System.EventHandler(this.btnInstalacion_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(341, 413);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(251, 90);
+            this.button2.TabIndex = 22;
+            this.button2.Text = "Mostrar los datos guardados en el archivo de instalacion";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowse.Location = new System.Drawing.Point(489, 55);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(103, 28);
+            this.btnBrowse.TabIndex = 23;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // btnBrowseB
+            // 
+            this.btnBrowseB.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBrowseB.Location = new System.Drawing.Point(489, 152);
+            this.btnBrowseB.Name = "btnBrowseB";
+            this.btnBrowseB.Size = new System.Drawing.Size(103, 28);
+            this.btnBrowseB.TabIndex = 24;
+            this.btnBrowseB.Text = "Browse";
+            this.btnBrowseB.UseVisualStyleBackColor = true;
+            this.btnBrowseB.Click += new System.EventHandler(this.btnBrowseB_Click);
+            // 
+            // lstDatosI
+            // 
+            this.lstDatosI.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lstDatosI.FormattingEnabled = true;
+            this.lstDatosI.ItemHeight = 22;
+            this.lstDatosI.Location = new System.Drawing.Point(622, 19);
+            this.lstDatosI.Name = "lstDatosI";
+            this.lstDatosI.Size = new System.Drawing.Size(336, 466);
+            this.lstDatosI.TabIndex = 25;
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(1034, 526);
+            this.Controls.Add(this.lstDatosI);
+            this.Controls.Add(this.btnBrowseB);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnInstalacion);
+            this.Controls.Add(this.cbxSisR);
+            this.Controls.Add(this.cbxOpc);
+            this.Controls.Add(this.txtIP);
+            this.Controls.Add(this.txtPC);
+            this.Controls.Add(this.txtLB);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtLI);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblM);
+            this.Controls.Add(this.lblSisR);
+            this.Controls.Add(this.lblPass);
+            this.Controls.Add(this.lblPCom);
+            this.Controls.Add(this.lblIP);
+            this.Controls.Add(this.lblLocalidadB);
+            this.Controls.Add(this.lblOpcI);
+            this.Controls.Add(this.lblLocalidadI);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblLocalidadI;
+        private System.Windows.Forms.Label lblOpcI;
+        private System.Windows.Forms.Label lblLocalidadB;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Label lblPCom;
+        private System.Windows.Forms.Label lblPass;
+        private System.Windows.Forms.Label lblSisR;
+        private System.Windows.Forms.Label lblM;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtLI;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtLB;
+        private System.Windows.Forms.TextBox txtPC;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.ComboBox cbxOpc;
+        private System.Windows.Forms.ComboBox cbxSisR;
+        private System.Windows.Forms.Button btnInstalacion;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.Button btnBrowseB;
+        private System.Windows.Forms.ListBox lstDatosI;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
     }
 }
 
--- TPV2doParcial/Form1.cs
+++ TPV2doParcial/Form1.cs
@@ -7,14 +7,142 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TPV2doParcial
 {
     public partial class Form1 : Form
     {
+        //VARIABLES
+            public string LOCALIDADINVENTARIO;
+            public string installoption;
+            public string LOCALIDADBASE;
+            public string installport;
+            public string installaddress;
+            public string installpassword;
+            public string installredundancia;
+            public bool val = false;
+
         public Form1()
         {
             InitializeComponent();
+            //LOCALIDAD DEL INVENTARIO POR DEFAULT
+            txtLI.Text = "/var/tmp";
+            //AGREGAR OPCIONES EN EL COMBOBOX DE OPCIONES DE INSTALACION
+            cbxOpc.Items.Add("FULL");//instalacion total(por defecto)
+            cbxOpc.Items.Add("PART");//instalcion parcial
+            cbxOpc.Items.Add("MIN");//instalcion minima
+            cbxOpc.Text = "FULL";
+            //LOCALIDAD BASE 
+            txtLB.Text = "/opt/base";
+            //puesto 8080
+            txtPC.Text = "8080";
+            //direccion IP 34.83.79.239
+            txtIP.Text = "34.83.79.79.239";
+            //AGREGAR OPCIONES EN EL COMBOBOX DE SISTEMA DE REDUNDANCIA
+            cbxSisR.Items.Add("NORMAL");
+            cbxSisR.Items.Add("HIGH");
+            cbxSisR.Items.Add("LOW");
+            cbxSisR.Text = "NORMAL";
+
+        }
+        
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            //Crear Cuadro de Seleccionar Carpeta
+            FolderBrowserDialog Carpeta = new FolderBrowserDialog();
+
+            //Si la carpeta ha sido elegida
+            if (Carpeta.ShowDialog() == DialogResult.OK)
+            {
+                //Carpeta que hemos seleccionado
+                txtLI.Text = Carpeta.SelectedPath;
+                LOCALIDADINVENTARIO = txtLI.Text;
+                //Mostrar
+                MessageBox.Show("La ruta seleccionada es: " + LOCALIDADINVENTARIO);
+            }
+        }
+
+        private void btnBrowseB_Click(object sender, EventArgs e)
+        {
+
+            //Crear Cuadro de Seleccionar Carpeta
+            FolderBrowserDialog Carpeta = new FolderBrowserDialog();
+
+            //Si la carpeta ha sido elegida
+            if (Carpeta.ShowDialog() == DialogResult.OK)
+            {
+                //Carpeta que hemos seleccionado
+                txtLB.Text = Carpeta.SelectedPath;
+                LOCALIDADBASE = txtLB.Text;
+                //Verificar si es correcto
+                MessageBox.Show("La ruta seleccionada es: " + LOCALIDADBASE);
+            }
+        }
+
+        private void btnInstalacion_Click(object sender, EventArgs e)
+        {
+            if (txtLI.Text == "")
+            {
+                errorProvider1.SetError(txtLI, "Favor de seleccionar una rura de inventario.");
+            }
+            else if(cbxOpc.Text==""){
+                errorProvider1.SetError(cbxOpc, "Favor de seleccionar una opcion de descarga.");
+            }
+            else if (txtLB.Text == "")
+            {
+                errorProvider1.SetError(txtLB, "Favor de seleccionar una rura base.");
+            }
+            else if (txtPassword.Text == "")
+            {
+                errorProvider1.SetError(txtPassword, "Favor de ingresar el password.");
+            }
+            else if (cbxSisR.Text == "")
+            {
+                errorProvider1.SetError(cbxSisR, "Favor de seleccionar una opcion de descarga.");
+            }
+            else
+            {
+                LOCALIDADINVENTARIO = txtLI.Text;
+                installoption = cbxOpc.Text;
+                LOCALIDADBASE = txtLB.Text;
+                installport = txtPC.Text;
+                installaddress = txtIP.Text;
+                installpassword = txtPassword.Text;
+                installredundancia = cbxSisR.Text;
+                TextWriter archivo;
+                archivo = new StreamWriter("DatosInstalacion.txt");
+                archivo.WriteLine("LOCALIDADINVENTARIO: " + LOCALIDADINVENTARIO);
+                archivo.WriteLine("install.option: " + installoption);
+                archivo.WriteLine("LOCALIDADBASE: " + LOCALIDADBASE);
+                archivo.WriteLine("install.port: " + installport);
+                archivo.WriteLine("install.address: " + installaddress);
+                archivo.WriteLine("install.password: " + installpassword);
+                archivo.WriteLine("install.redundancia: " + installredundancia);
+                archivo.Close();
+                MessageBox.Show("El archivo DatosInstalacion.txt ha sido creado.");
+                val = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (val == false)
+            {
+                errorProvider1.SetError(btnInstalacion, "No se a creado ningun archivo de instalacion aun.");
+            }
+            else
+            {
+                lstDatosI.Items.Add("*********************************************");
+                lstDatosI.Items.Add("LOCALIDADINVENTARIO: " + LOCALIDADINVENTARIO);
+                lstDatosI.Items.Add("install.option: " + installoption);
+                lstDatosI.Items.Add("LOCALIDADBASE: " + LOCALIDADBASE);
+                lstDatosI.Items.Add("install.port: " + installport);
+                lstDatosI.Items.Add("install.address: " + installaddress);
+                lstDatosI.Items.Add("install.password: " + installpassword);
+                lstDatosI.Items.Add("install.redundancia: " + installredundancia);
+                MessageBox.Show("Datos Mostrados");
+            }
         }
     }
 }
